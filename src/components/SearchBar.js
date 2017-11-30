48d01f2f529c1d234a995b217f63675f3b2a86bc
Juan C. Rodriguez <jcrodriguezw@gmail.com>
2017-11-30 10:25:27 -0500

    inicio

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+	render() {
+		return(
+			<form>
+				<input />
+				<button> buscar </button>
+			</form>
+			)
+	} 
+}
+export default SearchBar;
